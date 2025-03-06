0069225de86bf8a26b18a346fb081915f8a53e60
adamfinklecloudberry <198999961+adamfinklecloudberry@users.noreply.github.com>
2025-03-05 20:16:58 -0500

    Added login and registration and refactored app.py
    - Added a config.py to hold configuration
    - Added an extensions.py to hold extensions, namely BCrypt
    - Added a routes folder
    - Added a home route
    - Added an authentication route
    - Added a submissions route
    - Added SQLite database for testing
    - Added a User table
    - Added login.html for users to login
    - Added register.html for register.html

---
+++ extensions.py
@@ -0,0 +1,6 @@
+"""Module of extensions"""
+
+from flask_bcrypt import Bcrypt
+
+# Declared here to be initialized with the app elsewhere
+bcrypt = Bcrypt()
---
+++ models/user.py
@@ -0,0 +1,12 @@
+"""The user table"""
+
+from config import db
+
+
+class User(db.Model):
+    """User model"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)

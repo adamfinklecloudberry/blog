171766922415a15fa8fdfd0423c0d2ba77b4f3bc
adamfinklecloudberry <198999961+adamfinklecloudberry@users.noreply.github.com>
2025-03-07 15:44:00 -0500

    Added list of links to blogs of posters to main page

--- routes/home.py
+++ routes/home.py
@@ -4,6 +4,7 @@ The home route
 - Defines the home route for a Flask application
 """
 
+from models.user import User
 from flask import Blueprint, render_template
 import logging
 
@@ -15,4 +16,5 @@ home_blueprint = Blueprint("home", __name__)
 def home():
     """Main page of the blog"""
     logging.info("Home route accessed")
-    return render_template("index.html")
+    usernames = [user.username for user in User.query.all()]
+    return render_template("index.html", usernames=usernames)

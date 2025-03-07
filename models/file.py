064da0876188022b4fc68b4970ee7dee5affefdb
adamfinklecloudberry <198999961+adamfinklecloudberry@users.noreply.github.com>
2025-03-07 14:08:56 +0000

    Modernize the UI
    * Changed buttons to appear and disappear
    
    - Login and register appear when the user is not logged in
    - Logout and upload file appear when the user is logged in
    
    * Changed file list presentation to show user rather than user ID
    
    - Improved error handling
    - Changed styling on user files list page
    
    * Enabled intuitive blog browsing
    
    - Changed from user id based urls to username based urls
    - Required usernames to be unique
    - Displayed just the filename, without the extension or other url parts, in the file list of the user blog
    - Removed outdated methods
    
    * Changed blog font and narrowed blog width
    
    * Updated the user blog post list formatting
    
    - Moved the user name to the center on a dark grey banner
    - Moved the blog post links to the center in a list
    
    * Updated the appearance of index.html
    
    - Vertically centered the buttons in the banner
    - Changed the logout and upload a file buttons to be of uniform verticaly thickness
    - Renamed the upload a file button to upload a post
    
    * Upgraded the UI of upload.html
    
    - Switched boostrap
    - Centered the buttons
    
    * Upgraded the login UI with bootstrap and centered everything
    
    * Upgaded login.html with bootstrap and centered everything.
    
    * Added login and registration flash messages
    
    * Added flashes to submisions
    
    - Flash on successful upload
    - Flash on unsuccesful upload
    - Removed extraneous imports
    - Renamed header of view.html
    
    * Updated changelog

--- models/file.py
+++ models/file.py
@@ -7,5 +7,8 @@ class File(db.Model):
     """File database associated with users"""
 
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), unique=True, nullable=False)
+    filename = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    __table_args__ = (
+        db.UniqueConstraint("user_id", "filename", name="unique_user_filename"),
+    )
--- routes/submissions.py
+++ routes/submissions.py
@@ -3,7 +3,17 @@
 from config import s3, db
 from models.file import File
 from models.user import User
-from flask import Blueprint, request, render_template, current_app, url_for, jsonify
+from flask import (
+    Blueprint,
+    request,
+    render_template,
+    current_app,
+    flash,
+    url_for,
+    jsonify,
+    send_from_directory,
+    redirect,
+)
 from flask_login import login_required, current_user
 import boto3
 from werkzeug.utils import secure_filename
@@ -19,37 +29,49 @@ submissions_blueprint = Blueprint("submissions", __name__)
 @login_required
 def upload_file():
     """Route to handle file uploads"""
-    if not current_user.is_authenticated:
-        return "User is not authenticated", 401
-
     if request.method == "POST":
         if "file" not in request.files:
-            return "No file part"
+            flash("No file part", "danger")
+            return redirect(url_for("submissions.upload_file"))
         file = request.files["file"]
         if file.filename == "":
-            return "No selected file"
+            flash("No selected file", "danger")
+            return redirect(url_for("submissions.upload_file"))
         if file:
+            # Check if the file has a .txt extension
+            if not file.filename.lower().endswith(".txt"):
+                flash("Only .txt files are allowed", "danger")
+                return redirect(url_for("submissions.upload_file"))
+
             try:
                 # Secure the filename
                 original_filename = secure_filename(file.filename)
-                filename = original_filename
+                filename = f"{current_user.username}/{original_filename}"
 
-                # Check if the filename already exists in the database
-                existing_file = File.query.filter_by(filename=filename).first()
+                # Check if the file already exists for the user
+                existing_file = File.query.filter_by(
+                    user_id=current_user.id, filename=original_filename
+                ).first()
                 if existing_file:
-                    return "A file with this name already exists", 400
+                    flash("A blog post with the same name already exists.", "danger")
+                    return redirect(url_for("submissions.upload_file"))
 
                 # Create a new File instance and associate it with the current user
-                file_instance = File(filename=filename, user_id=current_user.id)
+                file_instance = File(
+                    filename=original_filename, user_id=current_user.id
+                )
                 db.session.add(file_instance)
                 db.session.commit()
 
                 # Upload to S3
                 s3.upload_fileobj(file, os.getenv("S3_BUCKET_NAME"), filename)
-                return "File uploaded successfully"
+                flash("Blog post uploaded successfully", "success")
+                return redirect(url_for("submissions.upload_file"))
             except ClientError as e:
-                return f"Error uploading file: {e}"
+                flash(f"Error uploading file: {e}", "danger")
+                return redirect(url_for("submissions.upload_file"))
     return render_template("upload.html")
+
 
 
 @submissions_blueprint.route("/download/<filename>")
@@ -62,62 +84,42 @@ def download_file(filename):
         return str(e)
 
 
-@submissions_blueprint.route("/view/<filename>")
-def view_file(filename):
-    """Route to handle file viewing"""
+@submissions_blueprint.route("/blog/<username>")
+def user_files(username):
+    # Query the database for the user
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        abort(404, description="User not found")
+
+    # Query the database for files associated with the user
+    files = File.query.filter_by(user_id=user.id).all()
+
+    # Create a list of file links without the .txt extension
+    file_links = [
+        f'/blog/{username}/{file.filename.rsplit(".", 1)[0]}' for file in files
+    ]
+
+    # Render the template with the file links
+    return render_template(
+        "user_files.html", user=user.username, file_links=file_links, error_message=None
+    )
+
+
+@submissions_blueprint.route("/blog/<username>/<filename>")
+def get_file(username: str, filename: str):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return "User not found", 404
+
+    # Generate the S3 key
+    s3_key = f"{username}/{filename}.txt"
+
     try:
-        # Get the bucket name from environment variables
-        bucket_name = os.getenv("S3_BUCKET_NAME")
-        if not bucket_name:
-            return "S3 bucket name is not set in environment variables", 500
         # Get the object from the S3 bucket
-        obj = s3.get_object(Bucket=bucket_name, Key=filename)
+        obj = s3.get_object(Bucket=os.getenv("S3_BUCKET_NAME"), Key=s3_key)
         # Read the file content
         file_content = obj["Body"].read().decode("utf-8")
         # Render the template with the file content
         return render_template("view.html", filename=filename, content=file_content)
     except Exception as e:
-        # Return the error message
-        return str(e), 500
-
-
-def get_user_files(user_id: int):
-    """Function to return links to all files associated with a user"""
-    # Get all files associated with the specified user
-    files = File.query.filter_by(user_id=user_id).all()
-
-    # Create a list of file links
-    file_links = [
-        url_for("submissions.view_file", filename=file.filename, _external=True)
-        for file in files
-    ]
-
-    # Return the list of file links as JSON
-    return jsonify(file_links)
-
-
-@submissions_blueprint.route("/user/<int:user_id>/files", methods=["GET"])
-def get_user_files_route(user_id: int):
-    """Route to return links to all files associated with a user"""
-    return get_user_files(user_id)
-
-
-@submissions_blueprint.route("/user/<int:user_id>/files_page", methods=["GET"])
-def user_files_page(user_id: int):
-    """Route to render the HTML page with file links"""
-    try:
-        # Call the get_user_files function directly
-        file_links = get_user_files(user_id).get_json()
-        error_message = ""
-    except Exception as e:
-        # Handle any exceptions
-        file_links = []
-        error_message = str(e)
-
-    # Render the HTML template with the file links
-    return render_template(
-        "user_files.html",
-        file_links=file_links,
-        user_id=user_id,
-        error_message=error_message,
-    )
+        return f"Error fetching file: {e}", 500
